cfc23f624fd4130f7e4ed20f1c9594a93a2c2a99
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-21 17:04:34 +0530

    Welcome to the Inventory Data Management

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagment
+{
+    class prgram
+    {
+        public static void Main(String[] args) 
+        {
+            Console.WriteLine("Welcome to the Inventory Data Management");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Welcome to the Inventory Data Management
