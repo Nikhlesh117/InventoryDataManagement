dacb33eab824d44ea1a02d7503d2bee6fa02485c
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-21 17:15:34 +0530

    Added: Program

--- Program.cs
+++ Program.cs
@@ -2,9 +2,50 @@
 {
     class prgram
     {
-        public static void Main(String[] args) 
+        public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to the Inventory Data Management");
+            string path = "D:\\Work\\Bridgelabz\\.net repo\\InventoryManagment\\InventoryManagment\\Inventory.json";
+            FetchInventory fetchInventory = new FetchInventory();
+
+            InventoryDetails data = fetchInventory.Read(path);
+            
+            Console.WriteLine("Type of Rice");
+            Console.WriteLine("******************************");
+            Console.WriteLine("******************************");
+            for (int i = 0; i < data.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(data.typesOfRice[i].name);
+                Console.WriteLine(data.typesOfRice[i].price);
+                Console.WriteLine(data.typesOfRice[i].weight);
+                Console.WriteLine("******************************");
+
+            }
+            Console.WriteLine("******************************");
+            Console.WriteLine("Type of Pulse");
+            Console.WriteLine("******************************");
+            Console.WriteLine("******************************");
+            for (int i = 0; i < data.typesOfPulse.Count; i++)
+            {
+                Console.WriteLine(data.typesOfPulse[i].name);
+                Console.WriteLine(data.typesOfPulse[i].price);
+                Console.WriteLine(data.typesOfPulse[i].weight);
+
+                Console.WriteLine("******************************");
+
+
+
+            }
+            Console.WriteLine("******************************");
+            Console.WriteLine("Type of Wheat");
+            Console.WriteLine("******************************");
+            Console.WriteLine("******************************");
+            for (int i = 0; i < data.typesOfWheat.Count; i++)
+            {
+                Console.WriteLine(data.typesOfWheat[i].name);
+                Console.WriteLine(data.typesOfWheat[i].price);
+                Console.WriteLine(data.typesOfWheat[i].weight);
+                Console.WriteLine("******************************");
+            }
         }
     }
 }
